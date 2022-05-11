6db409d09d41cbce234df0aad0707a687ad0902a
VronskyWebDev <alexey.vronsky.studio@gmail.com>
2022-05-11 18:42:34 +0300

    added flatpages to install apps

--- websearchtest/settings.py
+++ websearchtest/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+
 ]
 
 ROOT_URLCONF = 'websearchtest.urls'
